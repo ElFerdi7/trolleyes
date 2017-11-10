b5703cd4f26a74e5aba5b4a81b1878bbb58d1e02
a022583952e <a022583952e@S09-PC14>
2017-11-10 12:27:10 +0100

    Pedido listado

--- public_html/js/system/config/routes.js
+++ public_html/js/system/config/routes.js
@@ -52,6 +52,7 @@ trolleyes.config(['$routeProvider', function ($routeProvider) {
         //------------
         //$routeProvider.when('/tipousuario/1/selection/:page?/:rpp?', {templateUrl: 'js/app/tipousuario/1/selection.html', controller: 'TipousuarioSelection1Controller', resolve: {auth: authenticationAdministratorPromise}});                
         //--
-        $routeProvider.when('/pedido/1/view/:id', {templateUrl: 'js/app/pedido/1/view.html', controller: 'PedidoView1Controller', resolve: {auth: authenticationAdministratorPromise}});
+        $routeProvider.when('/pedido/1/plist/:page?/:rpp?', {templateUrl: 'js/app/pedido/1/plist.html', controller: 'PedidoPList1Controller', resolve: {auth: authenticationAdministratorPromise}});                
+
         $routeProvider.otherwise({redirectTo: '/'});
     }]);
